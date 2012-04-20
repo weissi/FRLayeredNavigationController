935522b613fb3a0333d6f19b2bd3c10822aebee2
Johannes Weiß <github@tux4u.de>
2012-04-20 13:46:23 +0200

    Initial Commit

---
+++ FancyVCTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FancyVCTest
+//
+//  Created by Johannes Weiß on 4/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
