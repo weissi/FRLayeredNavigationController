8d5624f4ad960b394c66379d054fb7467038cce2
Johannes Weiß <github@tux4u.de>
2012-04-29 12:51:03 +0200

    LICENSE (LGPL 3)

--- FancyVC/FancyChromeController.h
+++ FancyVC/FancyChromeController.h
@@ -1,10 +1,21 @@
-//
-//  FancyChromeController.h
-//  FancyVCTest
-//
-//  Created by Johannes Weiß on 4/23/12.
-//  Copyright (c) 2012 factis research GmbH. All rights reserved.
-//
+/*     This file is part of FancyVC.
+ *
+ * FancyVC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * FancyVC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with FancyVC.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *  Copyright (c) 2012, Johannes Weiß <weiss@tux4u.de> for factis research GmbH.
+ */
 
 #import <UIKit/UIKit.h>
 
