c6127606fae67cccc4659d22a7aef40c1f0f96c3
Johannes Weiß <github@tux4u.de>
2012-07-29 12:44:05 +0200

    DOC: updated documentation (displayShadow/enableUserInteraction)

---
+++ generate-html-documentation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+appledoc --create-html --project-name FRLayeredNavigationController \
+         --project-company 'factis research GmbH' \
+         --company-id com.factisresearch --output docs \
+         --no-create-docset FRLayeredNavigationController
